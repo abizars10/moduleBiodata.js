0a9508b77003150e5642a436a165f8c9654cd9e9
abizars10 <abizararrifai@gmail.com>
2023-05-12 13:41:52 +0700

    Membuat module biodata node.js

---
+++ biodata.js
@@ -0,0 +1,6 @@
+exports.biodata = function (Nama, Tempat, Tanggal, Alamat) {
+  return Nama;
+  return Tempat;
+  return Tanggal;
+  return Alamat;
+};
---
+++ moduleBiodata.js
@@ -0,0 +1,9 @@
+var biodata = require("./biodata");
+var Nama = "Abizar",
+  Tempat = "Bekasi",
+  Tanggal = 01 + " Oktober " + 1999,
+  Alamat = "JL. Ki Asmawi No. " + 10;
+console.log("Nama : " + biodata.biodata(Nama));
+console.log("Tempat Lahir : " + biodata.biodata(Tempat));
+console.log("Tanggal Lahir : " + biodata.biodata(Tanggal));
+console.log("Alamat : " + biodata.biodata(Alamat));
